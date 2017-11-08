ae7b9e0407c56fb251c943ef7525f8792f99e409
amongleaves <vaidotas.valiukas7@yandex.com>
2017-11-08 15:59:59 +0200

    Implement edit and update functionality (routes)

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,6 +2,7 @@ class PortfoliosController < ApplicationController
 	def index
 		@portfolio_items = Portfolio.all
 	end
+	
 	def new
 		@portfolio_item = Portfolio.new
 	end
@@ -16,5 +17,21 @@ class PortfoliosController < ApplicationController
       	format.html { render :new }
     	end
   	end
+  end
+  
+  def edit
+  	@portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+  	@portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtible, :body))
+        format.html { redirect_to portfolios_path, notice: 'This record was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 end
